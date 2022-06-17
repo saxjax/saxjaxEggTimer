99978f57e55664504daa7eab4d10964600b959f7
saxjax <saxjax@saxjax.dk>
2022-06-17 10:54:37 +0200

    Initial Commit

---
+++ Shared/saxjaxEggTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  saxjaxEggTimerApp.swift
+//  Shared
+//
+//  Created by Jakob Skov Søndergård on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct saxjaxEggTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
